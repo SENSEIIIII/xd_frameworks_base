b71475f67a62a2b6398cd0198626f2e1ebfafe81
Pranav Vashi <neobuddy89@gmail.com>
2020-02-28 00:19:59 +0530

    Alert Slider: Add toggle to disable notifications [1/2]
    Change-Id: If7aca1f3f52b338e74f504652ebf16e26d77a8fc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5716,6 +5716,12 @@ public final class Settings {
          * @hide
          */
         public static final String CHARGING_ANIMATION = "charging_animation";
+        
+         /**       
+         * Whether to show or hide alert slider notifications on supported devices
+         * @hide
+         */
+        public static final String ALERT_SLIDER_NOTIFICATIONS = "alert_slider_notifications";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
